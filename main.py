e4495f18ed06bd13997011735a19ce26c603f394
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2022-02-14 16:36:48 +0100

    initialize repository

---
+++ main.py
@@ -0,0 +1,69 @@
+from dash import Dash, html, dcc, Output, Input
+import plotly.express as px
+import dash_bootstrap_components as dbc
+import pandas as pd
+
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+sidebar = html.Div(
+    [
+        html.H2("Stackoverflow Survey", className="display-12"),
+        html.Hr(),
+        html.P(
+            "Stackoverflow 2021 survey using dash", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Page 1", href="/page-1", active="exact"),
+                dbc.NavLink("Page 2", href="/page-2", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return html.P("This is the content of the home page!")
+    elif pathname == "/page-1":
+        return html.P("This is the content of page 1. Yay!")
+    elif pathname == "/page-2":
+        return html.P("Oh cool, this is page 2!")
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+survey_df = pd.read_csv("data.csv")
+survey_df.head()
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
